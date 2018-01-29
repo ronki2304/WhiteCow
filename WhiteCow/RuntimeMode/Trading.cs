7a4458d4fc1eaa9aef1854c881e4308c67f70962
jeremy <jeremy.ronk@gmail.com>
2018-01-29 12:13:06 +0100

    merge accepted

--- WhiteCow/RuntimeMode/Trading.cs
+++ WhiteCow/RuntimeMode/Trading.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Configuration;
 using System.Threading;
 using WhiteCow.Broker;
@@ -9,81 +9,57 @@ using System.IO;
 
 namespace WhiteCow.RuntimeMode
 {
-    /// <summary>
-    /// the power of white cow
-    /// here is the trading intelligence
-    /// </summary>
-    public class Trading:IRuntimeMode
-    {
-        readonly Double ThresholdGap;
-        public TradingStep Step;
-<<<<<<< HEAD
-		
-        //trading plateform
-        Poloniex polo;
-		BitFinex btx;
-        public Trading()
-        {
-            ThresholdGap=Convert.ToDouble(ConfigurationManager.AppSettings["Runtime.ThresholdGap"]);
-			polo = new Poloniex();
-			btx = new BitFinex();
-=======
-        readonly Boolean LogToFile;
+	/// <summary>
+	/// the power of white cow
+	/// here is the trading intelligence
+	/// </summary>
+	public class Trading : IRuntimeMode
+	{
+		readonly Double ThresholdGap;
+		public TradingStep Step;
+		readonly Boolean LogToFile;
 		readonly string fileName = "Runtime.csv";
 
-        public Trading()
-        {
-            ThresholdGap = Convert.ToDouble(ConfigurationManager.AppSettings["Runtime.ThresholdGap"]);
-            LogToFile = Convert.ToBoolean(ConfigurationManager.AppSettings["Runtime.LogTicks"]);
+		public Trading()
+		{
+			ThresholdGap = Convert.ToDouble(ConfigurationManager.AppSettings["Runtime.ThresholdGap"]);
+			LogToFile = Convert.ToBoolean(ConfigurationManager.AppSettings["Runtime.LogTicks"]);
 
-            if (LogToFile)
-            {
-                String Header = "Date;BrHigh Name;BrHigh Last;BrHigh Ask;BrHigh Bid;BrLow Name;BrLow Last;BrLow Ask;BrLow Bid;state";
-                if (!File.Exists(fileName))
-                    File.AppendAllText(fileName, Header + Environment.NewLine);
-            }
->>>>>>> 311e4a3... add log to track the close position issue
-        }
-        /// <summary>
-        /// start trading engine
-        /// </summary>
-        public void StartToMooh()
-        {
-<<<<<<< HEAD
-          while (true)
-                TickGapAnalisys();
-=======
+			if (LogToFile)
+			{
+				String Header = "Date;BrHigh Name;BrHigh Last;BrHigh Ask;BrHigh Bid;BrLow Name;BrLow Last;BrLow Ask;BrLow Bid;state";
+				if (!File.Exists(fileName))
+					File.AppendAllText(fileName, Header + Environment.NewLine);
+			}
+		}
+		/// <summary>
+		/// start trading engine
+		/// </summary>
+		public void StartToMooh()
+		{
 
 			Poloniex polo = new Poloniex();
 			BitFinex btx = new BitFinex();
 
-            while (true)
-                TickGapAnalisys(polo,btx);
->>>>>>> 311e4a3... add log to track the close position issue
-            
-        }
+			while (true)
+				TickGapAnalisys(polo, btx);
+
+		}
 
 		/// <summary>
 		/// Analyse if the gap is enough to start trading
 		/// if not then wait
 		/// </summary>
-		private void TickGapAnalisys(Poloniex polo,BitFinex btx)
+		private void TickGapAnalisys(Poloniex polo, BitFinex btx)
 		{
-            Logger.Instance.LogInfo("Tick analysis");
-            Step = TradingStep.Tick;
-<<<<<<< HEAD
-			polo.RefreshWallet();
-			btx.RefreshWallet();
-           
-=======
-			
->>>>>>> 311e4a3... add log to track the close position issue
+			Logger.Instance.LogInfo("Tick analysis");
+			Step = TradingStep.Tick;
+
 			//tick analisys
 			Double gap = 0.0;
 			do
 			{
 				Logger.Instance.LogInfo("Gap is not enough wait 10sec");
-                Logger.Instance.LogInfo(Environment.NewLine);
 				Thread.Sleep(10000);
 
 				if (polo.LastTick == null)
@@ -94,26 +70,26 @@ namespace WhiteCow.RuntimeMode
 					continue;
 
 				//check if the broker answer in time to be efficient if not play again
-                if (Math.Abs(btx.LastTick.Timestamp - polo.LastTick.Timestamp) >= 4000)
+				if (Math.Abs(btx.LastTick.Timestamp - polo.LastTick.Timestamp) >= 4000)
 					continue;
 
 				if (polo.LastTick.Last > btx.LastTick.Last)
-                    gap = 100.0 * (polo.LastTick.Bid / btx.LastTick.Ask - 1.0);
+					gap = 100.0 * (polo.LastTick.Bid / btx.LastTick.Ask - 1.0);
 				else
-                    gap = 100.0 * (btx.LastTick.Bid / polo.LastTick.Ask - 1.0);
+					gap = 100.0 * (btx.LastTick.Bid / polo.LastTick.Ask - 1.0);
 
-                Logger.Instance.LogInfo($"Gap is {gap}%");
+				Logger.Instance.LogInfo($"Gap is {gap}");
 			} while (gap < ThresholdGap);
 
-            Logger.Instance.LogInfo("Gap is large enough take position");
+			Logger.Instance.LogInfo("Gap is large enough take position");
 
-		
 			if (polo.LastTick.Last > btx.LastTick.Last)
 				SetPosition(btx, polo);
 			else
 				SetPosition(polo, btx);
 
-
+			polo.RefreshWallet();
+			btx.RefreshWallet();
 		}
 
 		/// <summary>
@@ -123,18 +99,18 @@ namespace WhiteCow.RuntimeMode
 		/// <param name="BrHigh">High broker ticker, the short one</param>
 		private void SetPosition(Broker.Broker Brlow, Broker.Broker BrHigh)
 		{
-            Logger.Instance.LogInfo("taking position..");
-            Step = TradingStep.OpenPosition;
-            Double amount = Brlow.BaseWallet.amount < BrHigh.BaseWallet.amount ? Brlow.BaseWallet.amount : BrHigh.BaseWallet.amount;
-            Logger.Instance.LogInfo($"amount for trading is {amount} {Brlow.BaseWallet.currency}");
+			Logger.Instance.LogInfo("taking position..");
+			Step = TradingStep.OpenPosition;
+			Double amount = Brlow.BaseWallet.amount > BrHigh.BaseWallet.amount ? Brlow.BaseWallet.amount : BrHigh.BaseWallet.amount;
+			Logger.Instance.LogInfo($"amount for trading is {amount} {Brlow.BaseWallet.currency}");
 
-            Brlow.MarginBuy(amount);
-            BrHigh.MarginSell(amount);
+			Brlow.MarginBuy(amount);
+			BrHigh.MarginSell(amount);
 
-            if (LogToFile)
-                LogTicks(BrHigh, Brlow,"init position");
+			if (LogToFile)
+				LogTicks(BrHigh, Brlow, "init position");
 
-            Logger.Instance.LogInfo("Position done");
+			Logger.Instance.LogInfo("Position done");
 			ClosePosition(Brlow, BrHigh);
 		}
 		/// <summary>
@@ -144,9 +120,9 @@ namespace WhiteCow.RuntimeMode
 		/// <param name="BrHigh">High broker ticker, the short one</param>
 		private void ClosePosition(Broker.Broker Brlow, Broker.Broker BrHigh)
 		{
-            Logger.Instance.LogInfo("Now Waiting for the cross");
-            Step = TradingStep.ClosePosition;
-            double gap = Double.NaN;
+			Logger.Instance.LogInfo("Now Waiting for the cross");
+			Step = TradingStep.ClosePosition;
+			double gap = Double.NaN;
 			do
 			{
 				Logger.Instance.LogInfo($"Gap is is too large retry");
@@ -159,28 +135,23 @@ namespace WhiteCow.RuntimeMode
 				if (BrHigh.LastTick == null)
 					continue;
 
-<<<<<<< HEAD
-                gap = 100 * BrHigh.LastTick.Ask / Brlow.LastTick.Bid - 100;
-				Logger.Instance.LogInfo($"Gap is {gap}%");
-=======
-                //check if the broker answer in time to be efficient if not play again
+				//check if the broker answer in time to be efficient if not play again
 				if (Math.Abs(BrHigh.LastTick.Timestamp - Brlow.LastTick.Timestamp) >= 4000)
-                    continue;
+					continue;
 
-                gap = 100.0 * (BrHigh.LastTick.Ask / Brlow.LastTick.Bid - 1.0);
+				gap = 100.0 * (BrHigh.LastTick.Ask / Brlow.LastTick.Bid - 1.0);
 				Logger.Instance.LogInfo($"Gap is {gap}");
 				if (LogToFile)
-                    LogTicks(BrHigh, Brlow,"check close");
->>>>>>> 311e4a3... add log to track the close position issue
+					LogTicks(BrHigh, Brlow, "check close");
 
 			} while (gap > Convert.ToDouble(ConfigurationManager.AppSettings["Runtime.Closegap"]));
 
 			BrHigh.ClosePosition();
 			Brlow.ClosePosition();
 
-            Logger.Instance.LogInfo("position closed"); 
-             
-            //EquilibrateFund(Brlow,BrHigh);
+			Logger.Instance.LogInfo("position closed");
+
+			//EquilibrateFund(Brlow,BrHigh);
 		}
 		/// <summary>
 		/// Reequilibrate all broker
@@ -189,40 +160,40 @@ namespace WhiteCow.RuntimeMode
 		/// <param name="BrHigh">High broker ticker, the short one</param>
 		private void EquilibrateFund(Broker.Broker Brlow, Broker.Broker BrHigh)
 		{
-            Logger.Instance.LogInfo("Now transfer extra amount to reequilibrate");
-            Step = TradingStep.FundTransfer;
-            Brlow.RefreshWallet();
-            BrHigh.RefreshWallet();
-            Double amountToTransfer;
+			Logger.Instance.LogInfo("Now transfer extra amount to reequilibrate");
+			Step = TradingStep.FundTransfer;
+			Brlow.RefreshWallet();
+			BrHigh.RefreshWallet();
+			Double amountToTransfer;
 
-            //compute the amount to send
-            //two differents case similar treatment
-            if (Brlow.BaseWallet.amount>BrHigh.BaseWallet.amount)
-            {
-                amountToTransfer = Brlow.BaseWallet.amount - BrHigh.BaseWallet.amount - Brlow.GetWithDrawFees() / 2;
-                Brlow.Send(BrHigh._PublicAddress,amountToTransfer);
-                BrHigh.CheckReceiveFund(amountToTransfer);
-            }
-            else
-            {
-                amountToTransfer = BrHigh.BaseWallet.amount - Brlow.BaseWallet.amount - Brlow.GetWithDrawFees() / 2;
-                BrHigh.Send(Brlow._PublicAddress, amountToTransfer);
-                Brlow.CheckReceiveFund(amountToTransfer);
-            }
-            Logger.Instance.LogInfo($"full loop done {Environment.NewLine}Now go back to ticker analysis");
-            
+			//compute the amount to send
+			//two differents case similar treatment
+			if (Brlow.BaseWallet.amount > BrHigh.BaseWallet.amount)
+			{
+				amountToTransfer = Brlow.BaseWallet.amount - BrHigh.BaseWallet.amount - Brlow.GetWithDrawFees() / 2;
+				Brlow.Send(BrHigh._PublicAddress, amountToTransfer);
+				BrHigh.CheckReceiveFund(amountToTransfer);
+			}
+			else
+			{
+				amountToTransfer = BrHigh.BaseWallet.amount - Brlow.BaseWallet.amount - Brlow.GetWithDrawFees() / 2;
+				BrHigh.Send(Brlow._PublicAddress, amountToTransfer);
+				Brlow.CheckReceiveFund(amountToTransfer);
+			}
+			Logger.Instance.LogInfo($"full loop done {Environment.NewLine}Now go back to ticker analysis");
+
 		}
 
-        private void LogTicks(Broker.Broker BrHigh,Broker.Broker BrLow,String state )
-        {
-            String content = $"{DateTime.Now.ToString()};{BrHigh.Name.ToString()};{BrHigh.LastTick.Last};{BrHigh.LastTick.Ask};{BrHigh.LastTick.Bid};{BrLow.Name.ToString()};{BrLow.LastTick.Last};{BrLow.LastTick.Ask};{BrLow.LastTick.Bid};{state}";
+		private void LogTicks(Broker.Broker BrHigh, Broker.Broker BrLow, String state)
+		{
+			String content = $"{DateTime.Now.ToString()};{BrHigh.Name.ToString()};{BrHigh.LastTick.Last};{BrHigh.LastTick.Ask};{BrHigh.LastTick.Bid};{BrLow.Name.ToString()};{BrLow.LastTick.Last};{BrLow.LastTick.Ask};{BrLow.LastTick.Bid};{state}";
 
-            File.AppendAllText(fileName,content+Environment.NewLine);
-        }
+			File.AppendAllText(fileName, content + Environment.NewLine);
+		}
 
-        public void Dispose()
-        {
-            
-        }
-    }
+		public void Dispose()
+		{
+
+		}
+	}
 }
